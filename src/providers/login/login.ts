b45229be99876aee4bebc986a29634516cf32c68
rwfazul <rwfazul@inf.ufsm.br>
2018-09-17 20:18:47 -0300

    auth service

---
+++ src/providers/login/login.ts
@@ -0,0 +1,37 @@
+import { HttpClient, HttpHeaders  } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+/*
+  Generated class for the LoginProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json; charset=UTF-8'
+  })
+};
+
+@Injectable()
+export class LoginProvider {
+
+  private apiUrl = 'https://portal.ufsm.br';
+
+  constructor(public http: HttpClient) {
+    console.log('Hello LoginProvider Provider');
+  }
+
+  sendPostResquest(data) {
+	  return new Promise((resolve, reject) => {	  	
+	    this.http.post(this.apiUrl + 'SUBDOMAIN', JSON.stringify(data), httpOptions)
+	      .subscribe(res => {
+	        resolve(res);
+	      }, (err) => {
+	        reject(err);
+	      });
+	  });
+  }
+
+}
