9aa5cda049126096053efa40e9cefedb1d60d63b
rwfazul <rwfazul@inf.ufsm.br>
2018-09-17 20:10:42 -0300

    add admin route to test api

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 var express = require('express'),
     app = express();
+var extract = require('./services/extract-sheets');
+// view engine setup
+app.set('views', 'admin');
+app.set('view engine', 'ejs');
 
+app.use(express.static('admin'));
 app.use(express.static('www'));
 
 // CORS (Cross-Origin Resource Sharing) headers to support Cross-site HTTP requests
@@ -8,6 +13,16 @@ app.all('*', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     next();
+});
+
+app.get('/admin', function(req, res) {
+	extract(function(result, error){
+		if (error) res.render('index', { data: error });
+		else {
+			console.log(result[1]);
+			res.render('index', { data: result });
+		}
+	});
 });
 
 app.set('port', process.env.PORT || 5000);
