d1104cc6d0470117129728898edfc37ca7e185e1
rwfazul <rwfazul@inf.ufsm.br>
2018-10-04 01:41:03 -0300

    callback hell fixed

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,30 @@
 var express = require('express');
 var router = express.Router();
-var extract = require('../services/extract-sheets');
+var extractor = require('../services/google-sheets-api/extractor');
+
+var path = 'services/google-sheets-api/tokens';
+var sheets = {
+	servidores: {
+		sheetId: '1030iyP9Z4FZVIkFCwqcJ4HvCHj4-XQzxReyjbVM3Sr0',
+		sheetRange: 'Responses',
+		tokenPath: path + '/sheet-servidores.json'
+	},
+	salas: {
+		sheetId: '1Itqrdq8AENrc_yXG_IL3tDOaALRZYXEPWkqVC6wU6LI',
+		sheetRange: 'Responses',
+		tokenPath: path + '/sheet-salas.json'
+	},
+	solicitacoes: {
+		sheetId: '1Xyv_QKWnz-8QsFIHX5yTjQVoXL2IdK8SF6DB2kUeRlk',
+		sheetRange: 'Responses',
+		tokenPath: path + '/sheet-solicitacoes.json'
+	},
+	encaminhamentos: {
+		sheetId: '1jZJYoMaVuumahmEGP19a5BO4iNfKppMb-LvevLPtuPs',
+		sheetRange: 'Responses',
+		tokenPath: path + '/sheet-encaminhamentos.json'
+	}
+}
 
 router
 	.get('/', function (req, res) {
@@ -9,46 +33,24 @@ router
 	.get('/logout', function (req, res) {
 		res.render('admin/index');
 	})
-	.get('/dashboard', function (req, res) {
-		res.render('admin/dashboard');
+	.get('/dashboard', function (req, res) {      
+		res.render('admin/dashboard-base');
+	})
+	.get('/salas', function (req, res) {
+		getSheetData(sheets.salas, 'list-salas', res);
+	})
+	.get('/servidores', function (req, res) {
+		getSheetData(sheets.servidores, 'list-servidores', res);
+	})
+	.get('/solicitacoes', function (req, res) {
+		getSheetData(sheets.solicitacoes, 'list-solicitacoes', res);
+	})
+	.get('/encaminhamentos', function (req, res) {
+		getSheetData(sheets.encaminhamentos, 'list-encaminhamentos', res);
 	})
 	.get('/agenda', function (req, res) {
-		extract(function (result, error) {
-			if (error) res.render('admin/agenda', { data: error });
-			else res.render('admin/agenda', { data: result });
-		});
-	})
-	.get('/list-salas', function (req, res) {
-		extract(function (result, error) {
-			if (error) res.render('admin/list-salas', { data: error });
-			else res.render('admin/list-salas', { data: result });
-		});
-	})
-	.get('/list-servidores', function (req, res) {
-		extract(function (result, error) {
-			if (error) res.render('admin/list-servidores', { data: error });
-			else res.render('admin/list-servidores', { data: result });
-		});
-	})
-	.get('/list-solicitacoes', function (req, res) {
-		extract(function (result, error) {
-			if (error) res.render('admin/list-solicitacoes', { data: error });
-			else res.render('admin/list-solicitacoes', { data: result });
-		});
-	})
-	.get('/list-encaminhamentos', function (req, res) {
-		extract(function (result, error) {
-			if (error) res.render('admin/list-encaminhamentos', { data: error });
-			else res.render('admin/list-encaminhamentos', { data: result });
-		});
-	})
-	.get('/extract-sheets', function (req, res) {
-		extract(function (result, error) {
-			if (error) res.render('admin/table-extract-sheets', { data: error });
-			else res.render('admin/table-extract-sheets', { data: result });
-		});
+		res.render('admin/agenda');
 	});
-
 
 router.post('/login', function (req, res) {
 	var usuario = req.body['usuario'];
@@ -57,5 +59,19 @@ router.post('/login', function (req, res) {
 		res.render("admin/dashboard");
 	}
 });
+
+function getSheetData(sheet, page, res) {
+	extractor.getAuthClient(sheet, function(authClient, authUrl, credentialsErr) {
+	  if (credentialsErr) { console.log('credentialsErr:', credentialsErr); }
+	  else if (authUrl) { 
+	  	res.render('admin/dashboard-base', { page: page, authUrl: authUrl });
+	  }
+	  else {
+	    extractor.readSheet(authClient, sheet, function(result, err) {
+	    	res.render('admin/dashboard-base', { page: page, data: err ? err : result });
+	    })
+	  }
+	});
+}
 
 module.exports = router;
