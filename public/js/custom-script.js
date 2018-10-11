8340eced3c1aeddf8ed97423b78f4aca45cd294d
isasakis <isabellasakis@gmail.com>
2018-10-10 21:49:14 -0300

    table hours

--- public/js/custom-script.js
+++ public/js/custom-script.js
@@ -1,165 +1,89 @@
 
 
-	$(document).ready(function () {
+$(document).ready(function () {
 
-    $('.modal').modal();
-    $('.collapsible').collapsible();
-    $('.tooltipped').tooltip();
-    // page is now ready, initialize the calendar...
+  $('.modal').modal();
+  $('.collapsible').collapsible();
+  $('.tooltipped').tooltip();
+  // page is now ready, initialize the calendar...
 
-    $('#calendar').fullCalendar({
-      header: {
-        left: 'prev,next today',
-        center: 'title',
-        right: 'month,agendaWeek,agendaDay,listWeek'
-      },
-      defaultView: 'agendaWeek',
-      navLinks: true, // can click day/week names to navigate views
-      editable: true,
-      droppable: true, // this allows things to be dropped onto the calendar
-      eventLimit: true, // allow "more" link when too many events
-			weekends: false,
-			allDaySlot: false,
-			minTime: '08:00:00',
-			maxTime: '20:00:00',
-      events: [
-        {
-          title: 'Isabella',
-          start: '2018-09-27T14:00:00',
-          end: '2018-09-27T15:00:00',
-          constraint: 'fixed',
-          color: '#9c27b0'
-        },
-        {
-          id: 'fixed'
-        },
-        {
-          title: 'Rhauani',
-          start: '2018-09-25T10:00:00',
-          end: '2018-09-25T11:00:00',
-          color: '#4caf50'
-        },
-        {
-          start: '2018-09-24T00:00:00',
-          end: '2018-09-24T08:00:00',
-          overlap: false,
-          rendering: 'background',
-          color: '#ff9f89'
-        }
-      ],
-      drop: function () {
-        // is the "remove after drop" checkbox checked?
-        if ($('#drop-remove').is(':checked')) {
-          // if so, remove the element from the "Draggable Events" list
-          $(this).remove();
-        }
-      },
-      selectable: true,
-      selectHelper: true,
-      select: function (start, end) {
-        var title = prompt('Event Title:');
-        var eventData;
-        if (title) {
-          eventData = {
-            title: title,
-            start: start,
-            end: end
-          };
-          $('#calendar').fullCalendar('renderEvent', eventData, true); // stick? = true
-        }
-        $('#calendar').fullCalendar('unselect');
+  $('#calendar').fullCalendar({
+    header: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'month,agendaWeek,agendaDay,listWeek'
+    },
+    defaultView: 'agendaWeek',
+    navLinks: true, // can click day/week names to navigate views
+    editable: true,
+    droppable: true, // this allows things to be dropped onto the calendar
+    eventLimit: true, // allow "more" link when too many events
+    weekends: false,
+    allDaySlot: false,
+    minTime: '08:00:00',
+    maxTime: '20:00:00',
+    events: [
+      {
+        /*
+        id: 'restricaoIsabella',
+        start: '10:00:00',
+        end: '16:00:00',
+        rendering: 'background'
+        */
       }
-		});
-		
-	
+    ],
+    drop: function () {
+      // remove the element from the "Draggable Events" list
+      $(this).remove();
+    },
+    selectable: true,
+    selectHelper: true,
+    select: function (start, end) {
+      var title = prompt('Event Title:');
+      var eventData;
+      if (title) {
+        eventData = {
+          title: title,
+          start: start,
+          end: end
+        };
+        $('#calendar').fullCalendar('renderEvent', eventData, true); // stick? = true
+      }
+      $('#calendar').fullCalendar('unselect');
+    }
+  });
 
-    //ALUNOS QUE PREENCHEM FORMULÁRIO DE SOLICITAÇÃO - SEM COR (CINZA)
-    /* initialize the external events
------------------------------------------------------------------*/
-    $('#external-events .fc-event.grey').each(function () {
-      // store data so the calendar knows to render an event upon drop
-      $(this).data('event', {
-        title: $.trim($(this).text()), // use the element's text as the event title
-        stick: true, // maintain when user navigates (see docs on the renderEvent method)
-        color: '#9e9e9e'
-      });
-
-      // make the event draggable using jQuery UI
-      $(this).draggable({
-        zIndex: 999,
-        revert: true,      // will cause the event to go back to its
-        revertDuration: 0  //  original position after the drag
-      });
-
+  $('#external-events .blue').each(function () {
+    // store data so the calendar knows to render an event upon drop
+    $(this).data('event', {
+      title: $.trim($(this).text()), // use the element's text as the event title
+      duration: "01:00",
+      stick: true, // maintain when user navigates (see docs on the renderEvent method)
+      color: '#2196f3'
     });
-
-    //ALUNOS Resolução 33 / 2015) - COR AZUL
-    $('#external-events .blue').each(function () {
-      // store data so the calendar knows to render an event upon drop
-      $(this).data('event', {
-        title: $.trim($(this).text()), // use the element's text as the event title
-        stick: true, // maintain when user navigates (see docs on the renderEvent method)
-        color: '#2196f3'
-      });
-
-      // make the event draggable using jQuery UI
-      $(this).draggable({
-        zIndex: 999,
-        revert: true,      // will cause the event to go back to its
-        revertDuration: 0  //  original position after the drag
-      });
-
-    });
-
-    //ALUNOS EDUCAÇÃO ESPECIAL - COR VERDE
-    $('#external-events .green').each(function () {
-      // store data so the calendar knows to render an event upon drop
-      $(this).data('event', {
-        title: $.trim($(this).text()), // use the element's text as the event title
-        stick: true, // maintain when user navigates (see docs on the renderEvent method)
-        color: '#4caf50'
-      });
-
-      // make the event draggable using jQuery UI
-      $(this).draggable({
-        zIndex: 999,
-        revert: true,      // will cause the event to go back to its
-        revertDuration: 0  //  original position after the drag
-      });
-
-    });
-
-    //ALUNOS ENCAMINHADOS - COR AMARELO
-    $('#external-events .amber').each(function () {
-      // store data so the calendar knows to render an event upon drop
-      $(this).data('event', {
-        title: $.trim($(this).text()), // use the element's text as the event title
-        stick: true, // maintain when user navigates (see docs on the renderEvent method)
-        color: '#ffc107'
-      });
-
-      // make the event draggable using jQuery UI
-      $(this).draggable({
-        zIndex: 999,
-        revert: true,      // will cause the event to go back to its
-        revertDuration: 0  //  original position after the drag
-      });
-
+    // make the event draggable using jQuery UI
+    $(this).draggable({
+      zIndex: 999,
+      revert: true,      // will cause the event to go back to its
+      revertDuration: 0  //  original position after the drag
     });
 
   });
-  
-	/* Se for usado floating button
-	$(".action-btn").click(function () {
-	  var page = $(this).attr('id');
-	  $("#content").load(page + ".html");
-	})
-  
-	document.addEventListener('DOMContentLoaded', function () {
-	  var elems = document.querySelectorAll('.fixed-action-btn');
-	  var instances = M.FloatingActionButton.init(elems, {
-		direction: 'left'
-	  });
-	});
+
+  $('#external-events').on('mousedown', '.fc-event', function(){
+    $('#calendar').fullCalendar('renderEvent', {
+      id: "restricao",
+      start: '10:00:00',
+      end: '12:00:00',
+      dow: [1, 2, 3],
+      rendering: 'background'
+    });
   });
-  	*/
+
+  $('#external-events').on('mouseup', '.fc-event', function(){
+    $('#calendar').fullCalendar('removeEvents', [
+      "restricao"
+    ]);
+  });
+
+});
--- routes/admin.js
+++ routes/admin.js
@@ -28,7 +28,7 @@ router
 	.get('/logout', function (req, res) {
 		res.render('admin/index');
 	})
-	.get('/dashboard', function (req, res) {    
+	.get('/dashboard', function (req, res) {
 		res.render('admin/dashboard-base');
 	})
 	.get('/salas', function (req, res) {
@@ -44,17 +44,31 @@ router
 		return getSheetData(sheets.encaminhamentos, 'list-encaminhamentos', res);
 	})
 	.get('/agenda', function (req, res) {
-		extractor.getAuthClient(function(authClient, authUrl, credentialsErr) {
-		  if (credentialsErr) { console.log('credentialsErr:', credentialsErr); }
-		  else if (authUrl) { 
-		  	return res.render('admin/agenda', { authUrl: authUrl });
-		  }
-		  else {
-		    extractor.readSheet(authClient, sheets.solicitacoes, function(result, err) {
-		    	if (err) { console.log('extract err:', err); }
-		    	return res.render('admin/agenda', { data: result });
-		    })
-		  }
+		extractor.getAuthClient(function (authClient, authUrl, credentialsErr) {
+			if (credentialsErr) { console.log('credentialsErr:', credentialsErr); }
+			else if (authUrl) {
+				return res.render('admin/agenda', { authUrl: authUrl });
+			}
+			else {
+				extractor.readSheet(authClient, sheets.solicitacoes, function (result, err) {
+					if (err) { console.log('extract err:', err); }
+					return res.render('admin/agenda', { data: formatResult(result) });
+				})
+			}
+		});
+	})
+	.get('/test-solicitacoes', function (req, res) {
+		extractor.getAuthClient(function (authClient, authUrl, credentialsErr) {
+			if (credentialsErr) { console.log('credentialsErr:', credentialsErr); }
+			else if (authUrl) {
+				return res.render('admin/lists/test-solicitacoes', { authUrl: authUrl });
+			}
+			else {
+				extractor.readSheet(authClient, sheets.solicitacoes, function (result, err) {
+					if (err) { console.log('extract err:', err); }
+					return res.render('admin/lists/test-solicitacoes', { data: formatResult(result) });
+				})
+			}
 		});
 	});
 
@@ -65,10 +79,10 @@ router
 		if (usuario == "admin" && senha == "admin") {
 			res.render("admin/dashboard-base");
 		}
-		res.render("admin/index", { msg: 'credentialsErr'});
+		res.render("admin/index", { msg: 'credentialsErr' });
 	})
-	.post('/storeToken', function(req, res) {
-		extractor.setToken(req.body['token'], function(err) {
+	.post('/storeToken', function (req, res) {
+		extractor.setToken(req.body['token'], function (err) {
 			console.log('ERROR???');
 			if (err) console.log(err);
 			else console.log('NAO')
@@ -76,22 +90,90 @@ router
 		});
 	});
 
+
+function formatResult(result) {
+	var nomes = [];
+	var h1 = [];
+	var h2 = [];
+	var h3 = [];
+	var h4 = [];
+	var h5 = [];
+	var h6 = [];
+	var h7 = [];
+	var h8 = [];
+	var h9 = [];
+	var matrix = [];
+
+
+	if (result.length) {
+		for (var row = 0; row < result.length; row++) {
+			for (var col = 1; col < result[row].length; col++) {
+				switch (result[0][col]) {
+					case "Nome completo":
+						nomes[row] = result[row][col];
+					case "Horários livres [08h - 09h]":
+						h1[row] = result[row][col].split(",");
+					case "Horários livres [09h - 10h]":
+						h2[row] = result[row][col].split(",");
+					case "Horários livres [10h - 11h]":
+						h3[row] = result[row][col].split(",");
+					case "Horários livres [11h - 12h]":
+						h4[row] = result[row][col].split(",");
+					case "Horários livres [13h - 14h]":
+						h5[row] = result[row][col].split(",");
+					case "Horários livres [14h - 15h]":
+						h6[row] = result[row][col].split(",");
+					case "Horários livres [15h - 16h]":
+						h7[row] = result[row][col].split(",");
+					case "Horários livres [16h - 17h]":
+						h8[row] = result[row][col].split(",");
+					case "Horários livres [17h - 18h]":
+						h9[row] = result[row][col].split(",");
+
+				}
+			}
+		}
+		matrix.push(nomes);
+		matrix.push(h1);
+		matrix.push(h2);
+		matrix.push(h3);
+		matrix.push(h4);
+		matrix.push(h5);
+		matrix.push(h6);
+		matrix.push(h7);
+		matrix.push(h8);
+		matrix.push(h9);
+
+		
+		for (var row = 0; row < matrix.length; row++) {
+			for (var col = 0; col < matrix[row].length; col++) {
+				console.log("Linha ", row, "Coluna ", col, ":", matrix[row][col]);
+			}
+		}
+
+	}
+	
+	return matrix;
+}
+
 function getSheetData(sheet, page, res) {
-	extractor.getAuthClient(function(authClient, authUrl, credentialsErr) {
-	  if (credentialsErr) { console.log('credentialsErr:', credentialsErr); }
-	  else if (authUrl) { 
-	  	console.log('*** has url ***');
-	  	return res.render('admin/dashboard-base', { page: page, authUrl: authUrl });
-	  }
-	  else {
-	  	console.log('*** find token ***');
-	    extractor.readSheet(authClient, sheet, function(result, err) {
-	    	if (err) { console.log('extract err:', err); }
-	    	console.log('*** find data ***');
-	    	return res.render('admin/dashboard-base', { page: page, data: result });
-	    })
-	  }
+	extractor.getAuthClient(function (authClient, authUrl, credentialsErr) {
+		if (credentialsErr) { console.log('credentialsErr:', credentialsErr); }
+		else if (authUrl) {
+			console.log('*** has url ***');
+			return res.render('admin/dashboard-base', { page: page, authUrl: authUrl });
+		}
+		else {
+			console.log('*** find token ***');
+			extractor.readSheet(authClient, sheet, function (result, err) {
+				if (err) { console.log('extract err:', err); }
+				console.log('*** find data ***');
+				return res.render('admin/dashboard-base', { page: page, data: result });
+			})
+		}
 	});
 }
+
+
 
 module.exports = router;
