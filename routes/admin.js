3d68e28e38eff5c61f5281e5f88f9a5228c87d12
rwfazul <rwfazul@inf.ufsm.br>
2018-11-13 02:30:46 -0200

    remove unsed functions

--- routes/admin.js
+++ routes/admin.js
@@ -2,65 +2,41 @@ var express = require('express');
 var router = express.Router();
 var firestore = require('../services/firestore-api/firestore');
 
-const dashboardTemplate = 'dashboard_base';
 const listBase = 'lists/list-';
 
-/*
-// invoked for any requested passed to this router
-router.use(function(req, res, next) {
-  // middleware logic (auth)
-  next();
-});
-*/
-
 router
-	.get('/', function (req, res) {
-		res.render('admin/index');
-	})
-	.get('/logout', function (req, res) {
-		res.render('admin/index');
-	})
-	.get('/dashboard', function (req, res) {
+	.get(['/', '/dashboard'], function(req, res) {
+		console.log('-> /admin /admin/dashboard');
 		res.render('admin/dashboard');
 	})
-	.get('/salas', function (req, res) {
+	.get('/salas', function(req, res) {
 		returnResponse({ name: 'salas' }, res, 'list-base', listBase + 'salas');
 	})
-	.get('/profissionais', function (req, res) {
+	.get('/profissionais', function(req, res) {
 		returnResponse({ name: 'profissionais' }, res, 'list-base', listBase + 'profissionais');
 	})
-	.get('/solicitacoes', function (req, res) {
+	.get('/solicitacoes', function(req, res) {
 		returnResponse({ name: 'solicitacoes' }, res, 'list-base', listBase + 'solicitacoes');
 	})
-	.get('/encaminhamentos', function (req, res) {
+	.get('/encaminhamentos', function(req, res) {
 		returnResponse({ name: 'encaminhamentos' }, res, 'list-base', listBase + 'encaminhamentos');
 	})
-	.get('/agenda/profissionais', function (req, res) {
+	.get('/agenda/profissionais', function(req, res) {
 		returnResponse('', res, 'agenda-sala-picker');
 	})
-	.post('/agenda/profissionais/sala/:uri', function (req, res) {
+	.post('/agenda/profissionais/sala/:uri', function(req, res) {
 		var sala = { id: req.body['id'], nome: req.body['nome'] };
 		returnResponse({ sala: sala }, res, 'agenda-profissionais');
 	})
-	.get('/agenda/atendimentos', function (req, res) {
+	.get('/agenda/atendimentos', function(req, res) {
 		returnResponse('', res, 'agenda-sala-picker');
 	})
-	.post('/agenda/atendimentos/sala/:uri', function (req, res) {
+	.post('/agenda/atendimentos/sala/:uri', function(req, res) {
 		var sala = { id: req.body['id'], nome: req.body['nome'] };
 		returnResponse({ sala: sala }, res, 'agenda-atendimentos');
 	})
-	.get('/relatorios', function (req, res) {
+	.get('/relatorios', function(req, res) {
 		res.render('admin/relatorios');
-	});
-
-router
-	.post('/login', function (req, res) {
-		var usuario = req.body['usuario'];
-		var senha = req.body['senha'];
-		if (usuario == "admin" && senha == "admin") {
-			res.render("admin/dashboard-base");
-		}
-		res.render("admin/index", { msg: 'credentialsErr' });
 	});
 
 function returnResponse(data, res, page, partial) {
@@ -68,10 +44,6 @@ function returnResponse(data, res, page, partial) {
 	if (partial) response.partial = 'partials/' + partial;
 	if (data) response.data = data;
 	res.render('admin/' + page, response);
-}
-
-function errorHandler(res, err) {
-	res.render('errors/error_template', { page: 'extract-data', message: err });
 }
 
 module.exports = router;
